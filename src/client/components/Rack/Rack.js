c8727a15616f5a72e1cb634b5e4a628d488e61e6
AbhijathBenhur <abhijath.benhur@tellius.com>
2021-04-12 08:43:07 +0530

    Screens complete

---
+++ src/client/components/Rack/Rack.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Card, CardDeck, Image } from "react-bootstrap";
+import { useHistory } from 'react-router-dom';
+const Rack = (props) => {
+    let history = useHistory();
+    function openCardView(id){
+        history.push('/card/'+ id)
+    }
+
+    return (
+        <div  className="mt-4">
+            <h3>{props.category}</h3>
+            <CardDeck>
+                {props.cards.map((fingerprint,index) => {
+                    return (
+                    <Card key={index} onClick={() =>{
+                         openCardView(fingerprint.title)
+                    }}>
+                        <Card.Img variant="top" src={"https://source.unsplash.com/random/200x200?sig="+2} />
+                        <Card.Body>
+                            <Card.Title>{fingerprint.title}</Card.Title>
+                            <Card.Text>
+                                {fingerprint.category}
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            {/* <small className="text-muted">{fingerprint.supply} In supply</small> */}
+                            <small className="text-muted">{fingerprint.cost} ETH</small>
+                        </Card.Footer>
+                    </Card>
+                )})}
+                
+                </CardDeck>
+        </div>
+    );
+};
+export default Rack;
