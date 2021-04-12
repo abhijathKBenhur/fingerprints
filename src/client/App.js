f6cb10a58987d106af50f0f88e6586095328d99e
AbhijathBenhur <abhijath.benhur@tellius.com>
2021-04-12 14:03:36 +0530

    modularize interface

--- src/client/App.js
+++ src/client/App.js
@@ -1,100 +1,37 @@
-import React, { Component } from 'react';
-import Web3 from 'web3'
-import ipfs from './config/ipfs'
-import Fingerprint from './beans/Fingerprint'
-import './App.scss';
-import Fingerprints from '../abis/Fingerprints.json'
 
-import NFT from './screens/NFT/NFT'
+
+
+import BlockchainInterface from './interface/BlockchainInterface'
+
+import React, { Component } from 'react';
+import './App.scss';
+import ipfs from './config/ipfs'
+
+import NFTCard from './screens/NFTCard/NFTCard'
 import Gallery from './screens/gallery/gallery'
 import Header from './components/header/header'
 import { Container, Row, Col } from "react-bootstrap";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import {  Switch, Route } from "react-router-dom";
 
 class App extends Component {
   async componentWillMount() {
-    window.Fingerprints = Fingerprints
     window.ipfs = ipfs;
-    await this.loadWeb3()
-    await this.loadBlockchainData()
-    
+    window.BlockchainInterface = BlockchainInterface
+    BlockchainInterface.initialize().then(tokens => {
+      this.setState({
+        tokens
+      })
+    })
   }
-
-  async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-    }
-  }
-
-  async loadBlockchainData() {
-    const web3 = window.web3
-    // Load account
-    const accounts = await web3.eth.getAccounts();
-    this.setState({ account: accounts[0] });
-
-    const networkId = await web3.eth.net.getId()
-    const networkData = window.Fingerprints.networks[networkId];
-    if(networkData) {
-      const abi = window.Fingerprints.abi
-      const address = networkData.address
-      const contract = new web3.eth.Contract(abi, address)
-      this.setState({ contract })
-      const totalSupply = await contract.methods.totalTokens().call()
-      this.setState({ totalSupply })
-      for (var i = 1; i <= totalSupply; i++) {
-        const fingerprint = await contract.methods.metadataOf(i).call()
-        this.setState({
-          fingerprints: [...this.state.fingerprints, new Fingerprint(fingerprint)]
-        })
-      }
-    } else {
-      window.alert('Smart contract not deployed to detected network.')
-    }
-  }
-
-
 
   constructor(props) {
     super(props)
     this.state = {
-      account: '',
-      contract: null,
-      totalSupply: 0,
-      fingerprints: [],
+      tokens :[]
     }
     this.onSubmit = this.onSubmit.bind(this);
-    this.onUploadComplete = this.onUploadComplete.bind(this);
   }
 
-  onUploadComplete = ({options}) => {
-    let payLoad = {
-      account: this.state.account,
-      file: options.file,
-      name: options.tokenName,
-      category: options.tokenCategory,
-      amount: parseFloat(options.tokenSupply),
-      price:  window.web3.utils.toWei(options.tokenCost, 'ether'),
-      uri: options.file
-    }
-    this.state.contract.methods.mint(
-      payLoad.account, 
-      payLoad.name,
-      payLoad.category,
-      payLoad.amount,
-      payLoad.price,
-      payLoad.uri,
-      ).send({ from: this.state.account })
-    .once('receipt', (receipt) => {
-
-    })
-  }
 
   onSubmit(form) {
     const reader = new window.FileReader()
@@ -106,7 +43,7 @@ class App extends Component {
           return;
         }
         form.file = result[0].path
-        this.onUploadComplete({options:form}, this.onUploadComplete)
+        this.BlockchainInterface.createToken({options:form})
       })
     }
   }
@@ -120,10 +57,10 @@ class App extends Component {
               <Route
                 path='/home'
                 render={(props) => (
-                  <Gallery fingerprints={this.state.fingerprints} {...props} />
+                  <Gallery fingerprints={this.state.tokens} {...props} />
                 )}
               />
-              <Route path="/card/:id" children={<NFT />} />
+              <Route path="/card/:id" children={<NFTCard />} />
               <Route>{'404'}</Route>
             </Switch>
           </Container>
